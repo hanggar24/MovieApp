a35ba0599305a9608cdcc7094bb755960550fae6
hanggar24 <nggar.wahyu@gmail.com>
2021-06-08 22:13:56 +0700

    updated

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
           rel="noopener noreferrer"
           class="button--green"
         >
-          Documentation hanggar
+          Documentation hanggar wahyu
         </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
